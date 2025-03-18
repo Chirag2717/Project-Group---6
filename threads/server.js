0957011cc2861cb3a9834baac51719a52004ec5f
Chirag2717 <chirag.khurana@dcmail.ca>
2025-03-17 22:21:11 -0400

    Update server.js

--- threads/server.js
+++ threads/server.js
@@ -1,35 +1,8 @@
-const app = require('koa')();
-const router = require('koa-router')();
-const db = require('./db.json');
-
-// Log requests
-app.use(function *(next){
-  const start = new Date;
-  yield next;
-  const ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+    res.json({ message: "Threads Service is running!" });
 });
-
-router.get('/api/threads', function *() {
-  this.body = db.threads;
-});
-
-router.get('/api/threads/:threadId', function *() {
-  const id = parseInt(this.params.threadId);
-  this.body = db.threads.find((thread) => thread.id == id);
-});
-
-router.get('/api/', function *() {
-  this.body = "API ready to receive requests";
-});
-
-router.get('/', function *() {
-  this.body = "Ready to receive requests";
-});
-
-app.use(router.routes());
-app.use(router.allowedMethods());
-
-app.listen(3000);
-
-console.log('Worker started');
+ 
+app.listen(4001, () => console.log('Threads Service running on port 4001'));
